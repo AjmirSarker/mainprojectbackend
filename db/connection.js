e4d3d4ee854678efdee1fd949e824b961319b92b
Niloy173 <niloysaha173@gmail.com>
2023-05-02 01:58:35 +0600

    Connected through localDB

--- db/connection.js
+++ db/connection.js
@@ -4,18 +4,18 @@ const mongoose = require('mongoose');
 const connect = () => {
 
   mongoose.set("strictQuery", false);
-  mongoose.connect(process.env.MONGO_URL,{
+  mongoose.connect(process.env.LOCAL_DB,{
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log(`Connection Established Successfully`);
   })
   .catch((err) => console.log(err));
 
-  mongoose.connection.on("disconnected", () => {
-    console.log("Connection Disconnected or Interrupted");
-  })
+  // mongoose.connection.on("disconnected", () => {
+  //   console.log("Connection Disconnected or Interrupted");
+  // })
 }
 
 module.exports = {
