c50e67bd61cfbe6f89f8874d109b4d8d5f090391
Niloy173 <niloysaha173@gmail.com>
2023-05-02 02:00:36 +0600

    Local connection specified for testing

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const connect = () => {
 
   mongoose.set("strictQuery", false);
-  mongoose.connect(process.env.LOCAL_DB,{
+  mongoose.connect(process.env.LOCAL_DB,{ //mongodb://localhost:27017/pcparts
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
