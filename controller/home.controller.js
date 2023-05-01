af0a19d131a64a71a2e327261bc86b0c4fa19c98
Niloy173 <niloysaha173@gmail.com>
2023-05-02 02:01:42 +0600

    functionality added for findAll, PostSingle, GetSpecific product

--- controller/home.controller.js
+++ controller/home.controller.js
@@ -1,10 +1,24 @@
 /* load requried models */
-
+const ProductModel = require('../model/product.model');
 /* load dependencies, external files */
+const {CreateError} = require('../helper/CreateError');
 
 const FindAllProduct = (req, res, next) => {
 
-  res.send("Fetching all products...");
+  // try {
+    // const data = await new ProductModel().GetAllProducts();
+  //  res.send(data);
+  // } catch (error) {
+  //   next(CreateError(error));
+  // }
+  new ProductModel().GetAllProducts()
+  .then((products) => {
+    res.send(products)
+  })
+  .catch((err) => {
+    next(CreateError(err))
+  })
+  
 }
 
 const NewMessage = (req, res, next) => {
@@ -13,7 +27,49 @@ const NewMessage = (req, res, next) => {
   
 }
 
+const addNewProduct = (req, res, next) => {
+
+  const SingleCasing = {
+    "Model": "Golden Field F22B ATX Desktop Case",
+    "Fan": "80MM fan at rear side.",
+    "Price": "2200",
+    "Brand": "Golden Field",
+    "Weight": "4.6 KG",
+    "Details": "Solid mechanical structure,Dual USB 2.0, audio ports and the front, easy access",
+    "Dimension": "373x182x415mm",
+    "Identity": "Casing",
+    "ProductId": "1001",
+    "img1": "https://i.ibb.co/YdLXgcD/Golden-Field-F22-B-ATX-Desktop-Case-1.png",
+    "img2": "https://i.ibb.co/vjVSNN5/Golden-Field-F22-B-ATX-Desktop-Case-2.png"
+  }
+
+  
+
+  try {
+    const newProduct = new ProductModel().InsertProduct(SingleCasing);
+    newProduct.save();
+
+    res.send("New Product saved successfully");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+const GetSpecifcProduct = async (req, res,next) => {
+  
+  try {
+      const Identity = req.params.identity;
+      const data = await ProductModel.FindByIdentity(Identity);
+      res.send(data);
+  } catch (error) {
+    next(CreateError(error));
+  }
+}
+
 module.exports = {
   FindAllProduct,
-  NewMessage
+  NewMessage,
+  addNewProduct,
+  GetSpecifcProduct
 }
