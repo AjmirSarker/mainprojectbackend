bbf357f3ea68b15dfce2005cd6f2361f6d2aaf5f
Niloy173 <niloysaha173@gmail.com>
2023-05-03 12:03:48 +0600

    navbar has been designed successfully

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Navbar from './components/Navbar';
+
+/* default style sheet */
+import './App.css';
+
+/* main style sheet */
+import './style/style.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
